b4a7b71bc8df7fe1ccbe014eb6feb4c89280f3e5
pal_user <fcs@ford.com>
2018-11-13 14:16:23 -0700

    12 Factor Applications LAB

--- src/main/java/io/pivotal/pal/tracker/WelcomeController.java
+++ src/main/java/io/pivotal/pal/tracker/WelcomeController.java
@@ -1,13 +1,20 @@
 package io.pivotal.pal.tracker;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class WelcomeController {
+    public String helloMessage;
+
     @GetMapping("/")
     public String sayHello() {
-      return "hello";
+        return helloMessage;
+    }
+
+    public WelcomeController(@Value("${WELCOME_MESSAGE}") String message) {
+        helloMessage = message;
     }
 
 }
